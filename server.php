c2adbb24b2d7204bfe6ded9054469860e9d974b1
gvswe <75132744+gvswe@users.noreply.github.com>
2020-12-03 21:05:17 +0530

    Add files via upload

---
+++ server.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+include('connect.php');
+
+
+// initializing variables
+$name = "";
+$email = "";
+$rno = "";
+$pno = "";
+$add = "";
+$pos = "";
+$fee = "";
+$dues = "";
+$password = "";
+$course = "";
+$errors = array(); 
+$sem = "";
+
+
+// ... 
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+  $rno = mysqli_real_escape_string($db, $_POST['rno']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($rno)) {
+      array_push($errors, "rollno is required");
+  }
+  if (empty($password)) {
+      array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+      $query = "SELECT * FROM logininfo WHERE rollno='$rno' AND password='$password'";
+      $results = mysqli_query($db, $query);
+      if (mysqli_num_rows($results) == 1) {
+        $_SESSION['rollno'] =$rno;
+        header('location: register.php');
+      }else {
+          array_push($errors, "Wrong username/password combination");
+      }
+  }
+}
+
+
+    // REGISTER USER
+    if (isset($_POST['reg_user'])) {
+      // receive all input values from the form
+      $fee = mysqli_real_escape_string($db, $_POST['fee']);
+      $dues = mysqli_real_escape_string($db, $_POST['dues']);
+      $pos = mysqli_real_escape_string($db, $_POST['pos']);
+      $sem = mysqli_real_escape_string($db, $_POST['sem']);
+      $course = mysqli_real_escape_string($db, $_POST['course']);
+
+
+      if (empty($pos)){ 
+        array_push($errors, "*Programme of Study is required");
+        }
+      if (empty($fee)){ 
+        array_push($errors, "*Check the INSTITUTE/HOSTEL FEES field");
+        }
+      if (empty($dues)) { 
+        array_push($errors, "*Check the OTHER DUES field");
+        }
+      if (empty($sem)) { 
+        array_push($errors, "*Select the SEMESTER");
+        }
+
+        if($_POST['sem'] == -1){
+        array_push($errors, "*Please select Semester");
+        }
+      if (count($errors) == 0) {
+        $query = "INSERT INTO tempdetails (COURSE_ID, COURSE_NAME, COURSE_CREDIT) 
+  			  VALUES('cid', 'cname', 'cr')";
+
+        $_SESSION['prgofstudy'] =$pos;
+        $_SESSION['feestatus'] =$fee;
+        $_SESSION['otherdues'] =$dues;
+        $_SESSION['semester'] =$sem;
+        $_SESSION['course'] =$course;
+
+        header('location: confirmpage.php');
+   
+        }
+
+      
+
+
+
+      }
+
+
+
+  
+  ?>
